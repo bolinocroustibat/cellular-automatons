2dcedfde0d82d5137d9a30fb92a7418c6984afef
Adrien Carpentier <me@adriencarpentier.com>
2024-01-26 00:06:22 +0100

    build: add vite config to build with relative path

---
+++ vite.config.js
@@ -0,0 +1,6 @@
+// vite.config.js
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+    base: '' // empty string = relative path (https://stackoverflow.com/questions/69744253/vite-build-always-using-static-paths)
+})
