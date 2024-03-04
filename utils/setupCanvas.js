fecc5ea02bd4f9716f8f46bc4faa4a3d04ad652f
Adrien Carpentier <me@adriencarpentier.com>
2024-03-04 14:58:38 +0100

    fix: remove scrollbars on canvas

--- utils/setupCanvas.js
+++ utils/setupCanvas.js
@@ -1,8 +1,13 @@
 export const setupCanvas = (canvasEl, width, height) => {
+	/**
+	 * @param {HTMLCanvasElement} canvasEl
+	 * @param {number} width
+	 * @param {number} height
+	 * https://stackoverflow.com/questions/4938346/canvas-width-and-height-in-html5
+	 * If you don't set the CSS attributes, the intrinsic size of the canvas will be used as its display size; if you do set the CSS attributes, and they differ from the canvas dimensions, your content will be scaled in the browser.
+	**/
 	canvasEl.width = width
 	canvasEl.height = height
-	canvasEl.style.width = `${width}px`
-	canvasEl.style.height = `${height}px`
 	canvasEl.style.margin = "auto"
 	const ctx = canvasEl.getContext("2d")
 	const img = new Image()
