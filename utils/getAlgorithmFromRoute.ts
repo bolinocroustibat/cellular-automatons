b3ab5f4c1872a10228cd9d0f6a812c6a807fb69f
Adrien Carpentier <me@adriencarpentier.com>
2025-02-12 21:12:11 +0900

    clean: lint code

--- utils/getAlgorithmFromRoute.ts
+++ utils/getAlgorithmFromRoute.ts
@@ -1,18 +1,20 @@
 const VALID_ALGOS = [
-    "cca-1D",
-    "cca-2D",
-    "cca-3D",
-    "conway",
-    "immigration",
-    "quadlife",
-    "langton",
-    "entropy",
-    "rule30",
-    "rule90",
-    "rule110",
+	"cca-1D",
+	"cca-2D",
+	"cca-3D",
+	"conway",
+	"immigration",
+	"quadlife",
+	"langton",
+	"entropy",
+	"rule30",
+	"rule90",
+	"rule110",
 ] as const
 
 export function getAlgorithmFromRoute(): string {
-    const path = window.location.pathname.slice(1)
-    return VALID_ALGOS.includes(path as typeof VALID_ALGOS[number]) ? path : "cca-2D"
+	const path = window.location.pathname.slice(1).toLowerCase()
+	return VALID_ALGOS.includes(path as (typeof VALID_ALGOS)[number])
+		? path
+		: "cca-2D"
 }
